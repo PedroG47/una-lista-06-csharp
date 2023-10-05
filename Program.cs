68bea7944dd0236f4f2331b2078371f87fcdb56b
PedroG47 <49578117+PedroG47@users.noreply.github.com>
2023-10-04 21:41:40 -0300

    ex2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       int[] vetor = new int[] { 1, 2, 3, 4, 5 };
+
+       int[,] matriz = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+    }
+
+}
